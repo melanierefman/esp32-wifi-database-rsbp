a7a4384ac51f49f8fb3e98de5f1f476a5106e708
melanierefman <87106838+melanierefman@users.noreply.github.com>
2023-12-10 20:38:55 +0700

    update file.ino

--- sketch_dec10a.ino
+++ sketch_dec10a.ino
@@ -1,10 +1,10 @@
 #include <WiFi.h> 
 #include <HTTPClient.h> 
 
-String URL = "http://192.168.1.26/esp32/database.php";
+String URL = "http:// 192.168.144.63/esp32/database.php";
 
-const char* ssid = "ZTE_plus"; 
-const char* password = "Kirana29"; 
+const char* ssid = "Melanie's Galaxy A52"; 
+const char* password = "a12345678";
 
 void setup() {
   Serial.begin(9600); 
@@ -18,12 +18,19 @@ void loop() {
 
   int data1 = random(35, 37); 
   int data2 = random(150, 180);
-  int data3 = random(120,130);
-  int data4 = random(60,100);
-  int data5 = random(40,70);
+  int data3 = random(120, 130);
+  int data4 = random(60, 100);
+  int data5 = random(40, 70);
+  int data6 = random(0, 200); // Contoh data6
+  int data7 = random(170, 250); // Contoh data7
 
-  // String postData = "data1=" + String(data1) + " data2=" + String(data2) + " data5=" + String(data3) + "/" + String(data4);
-  String postData = "data1=" + String(data1) + "&data2=" + String(data2) + "&data3=" + String(data3) + "&data4=" + String(data4) + "&data5=" + String(data5);
+  String postData = "data1=" + String(data1) +
+                    "&data2=" + String(data2) +
+                    "&data3=" + String(data3) +
+                    "&data4=" + String(data4) +
+                    "&data5=" + String(data5) +
+                    "&data6=" + String(data6) + // Menambahkan data6
+                    "&data7=" + String(data7); // Menambahkan data7
 
   HTTPClient http; 
   http.begin(URL);
@@ -43,7 +50,7 @@ void loop() {
 
 void connectWiFi() {
   WiFi.mode(WIFI_OFF);
-  delay(5000);
+  delay(10000);
   //This line hides the viewing of ESP as wifi hotspot
   WiFi.mode(WIFI_STA);
   
@@ -57,4 +64,4 @@ void connectWiFi() {
     
   Serial.print("connected to : "); Serial.println(ssid);
   Serial.print("IP address: "); Serial.println(WiFi.localIP());
-}
+} 
